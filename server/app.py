29c7687a7c9a6c7f0337e6ae82b236f0607a17c3
James Fields <fieldsjay13@gmail.com>
2023-05-24 15:33:59 -0600

    broken tables

--- server/app.py
+++ server/app.py
@@ -1,16 +1,117 @@
-#!/usr/bin/env python3
+from flask import Flask, request, jsonify, make_response, session
 
-# Standard library imports
+from config import app, db
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
+from models import db, User, Brewery, Review, Favorite
 
-# Local imports
-from config import app, db, api
-from models import User, Recipe
 
-# Views go here!
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/signupuser', methods=['POST'])
+def signup_user():
+    request_json = request.get_json()
+    
+        
+    name = request_json.get('name')
+    email = request_json.get('email')
+    new_user = User(
+        name = name,
+        email = email,
+        )
+
+    password = request_json.get('password')
+    new_user.password_hash = password
+    db.session.add(new_user)
+    db.session.commit()
+    session['user_id'] = new_user.id
+    session['user_type'] = 'user'
+    
+    return new_user.to_dict()
+
+@app.route('/check_session', methods=['GET'])
+def check_session():
+    # user = None
+    if session.get('user_id'):
+        if session.get('user_type') == 'user':
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+
+    return {'error': '401 Unauthorized'}, 401
+
+@app.route('/login', methods=['POST'])
+def login():
+    request_json = request.get_json()
+    email = request_json.get('email')
+    password = request_json.get('password')
+    check_for_influencer_account = User.query.filter(User.email == email).first()
+
+@app.route('/logout', methods=['DELETE'])
+def logout():
+    
+    if session.get('user_id'):
+        session['user_id'] = None
+        return {}, 204
+    return {'error': '401 Unauthorized'}, 401
+
+@app.route('/users', methods=['GET', 'POST'])
+def user_index():
+    if request.method == 'GET':
+        users = User.query.all()
+        return jsonify([i.to_dict() for i in users])
+    elif request.method == 'POST':
+        new_user = User(**request.json)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify(new_user.to_dict()), 201
+
+    
+@app.route('/users/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def users_show(id):
+    user = User.query.get(id)
+    if request.method == 'GET':
+        return jsonify(user.to_dict())
+    elif request.method == 'PATCH':
+        for key, value in request.json.items():
+            setattr(user, key, value)
+        db.session.commit()
+        return jsonify(user.to_dict())
+    elif request.method == 'DELETE':
+        db.session.delete(user)
+        db.session.commit()
+        return make_response('', 204)
+    
+@app.route('/breweries', methods=['GET', 'POST'])
+def breweries_index():
+    if request.method == 'GET':
+        brewery = Brewery.query.all()
+        return jsonify([c.to_dict() for c in brewery])
+    elif request.method == 'POST':
+        new_brewery = Brewery(**request.json)
+        db.session.add(new_brewery)
+        db.session.commit()
+        return jsonify(new_brewery.to_dict()), 201
+    
+@app.route('/breweries/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def breweries_show(id):
+    brewery = Brewery.query.get(id)
+    if request.method == 'GET':
+        return jsonify(brewery.to_dict())
+    elif request.method == 'PATCH':
+        for key, value in request.json.items():
+            setattr(brewery, key, value)
+        db.session.commit()
+        return jsonify(brewery.to_dict())
+    elif request.method == 'DELETE':
+        db.session.delete(brewery)
+        db.session.commit()
+        return make_response('', 204)
+
+
+
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,83 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
 
-# Models go here!
+from config import app, db, bcrypt
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    username = db.Column(db.String)
+    password_hash = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+
+class Brewery(db.Model, SerializerMixin):
+    __tablename__ = 'breweries'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+    longitude = db.Column(db.Float)
+    latitude = db.Column(db.Float)
+    hasFood = db.Column(db.Boolean)
+    allowsDogs = db.Column(db.Boolean)
+    hasLiveMusic = db.Column(db.Boolean)
+    hasPoolTables = db.Column(db.Boolean)
+    hasTriviaNight = db.Column(db.Boolean)
+    websiteUrl = db.Column(db.String)
+    phoneNumber = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+  
+
+
+class Favorite(db.Model, SerializerMixin):
+    __tablename__ = 'favorites'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    brewery_id = db.Column(db.Integer, db.ForeignKey('breweries.id'))
+  
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    brewery_id = db.Column(db.Integer, db.ForeignKey('breweries.id'))
+    content = db.Column(db.Text)
+    title = db.Column(db.String)
+    
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
